47d7c9a927e80057983e96e8bb8cad429b266e2a
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2022-02-16 16:37:20 +1100

    Update handling of SecretsManager secrets

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import requests
 import os
 import boto3
 
-omw_api_key = os.environ.get("OMW_API_KEY")
+secretsmanagerclient = boto3.client("secretsmanager")
 snsclient = boto3.client("sns")
-topic = snsclient.Topic(os.environ.get("SNS_TOPIC_ARN"))
+
+topic_arn = os.environ.get("SNS_TOPIC_ARN")
+LAT = -35.2809
+LON = 149.1300
+
+omw_api_key = secretsmanagerclient.get_secret_value(
+    SecretId=os.environ.get("OMW_API_KEY_SECRETSMANAGER_ARN")
+).get("SecretString")
 
 
 def lambda_handler(event, lambda_context):
     # Arguments
-    phone_number = event["phone_number"]
-    latitude_degrees = event["latitude_degrees"]
-    longitude_degrees = event["longitude_degrees"]
+    latitude_degrees = LAT
+    longitude_degrees = LON
 
     OPEN_WM = "https://api.openweathermap.org/data/2.5/onecall"
     parameters = {
@@ -38,12 +44,14 @@ def lambda_handler(event, lambda_context):
             count += 1
 
     if count > 0:
-        response = topic.publish(
-            PhoneNumber=phone_number,
+        sns_response = snsclient.publish(
+            TargetArn=topic_arn,
             Message="Its going to rain today! â›ˆ Make sure you bring an â˜‚ï¸!",
         )
+        return {
+            "statusCode": 200,
+            "body": json.dumps(f"Success. Text message sent to {phone_number}."),
+        }
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(f"Success. Text message sent to {phone_number}."),
-    }
+    else:
+        return {"statusCode": 200, "body": json.dumps("No action required.")}

3cf58772bbe477f43c1cf4039034978fab4766ac
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2022-02-16 17:26:23 +1100

    Update lambda function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-import requests
-import os
 import boto3
+import datetime
+import json
+import os
+import requests
 
 secretsmanagerclient = boto3.client("secretsmanager")
 snsclient = boto3.client("sns")
@@ -13,12 +14,14 @@ LON = 149.1300
 omw_api_key = secretsmanagerclient.get_secret_value(
     SecretId=os.environ.get("OMW_API_KEY_SECRETSMANAGER_ARN")
 ).get("SecretString")
+omw_api_key = json.loads(omw_api_key).get("ApiKey")
 
 
 def lambda_handler(event, lambda_context):
     # Arguments
     latitude_degrees = LAT
     longitude_degrees = LON
+    print(omw_api_key)
 
     OPEN_WM = "https://api.openweathermap.org/data/2.5/onecall"
     parameters = {
@@ -43,14 +46,25 @@ def lambda_handler(event, lambda_context):
         if i < 700:
             count += 1
 
-    if count > 0:
+    if not count > 0:
+        tomorrow = datetime.datetime.today() + datetime.timedelta(hours=10)
+        current_date_string = tomorrow.strftime("%d-%m-%Y")
         sns_response = snsclient.publish(
-            TargetArn=topic_arn,
-            Message="Its going to rain today! â›ˆ Make sure you bring an â˜‚ï¸!",
+            TopicArn=topic_arn,
+            Message=f"😮 Its going to rain today ({current_date_string})! Make sure you bring an ☂",
+            MessageAttributes={
+                "AWS.SNS.SMS.SenderID": {
+                    "DataType": "String",
+                    "StringValue": "rainbyram",
+                }
+            },
         )
+        print(sns_response)
         return {
             "statusCode": 200,
-            "body": json.dumps(f"Success. Text message sent to {phone_number}."),
+            "body": json.dumps(
+                f"Success. Text message sent to all subscribers via SMS."
+            ),
         }
 
     else:
